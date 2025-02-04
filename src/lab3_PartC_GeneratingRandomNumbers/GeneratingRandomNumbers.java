f58e7d0430ba6f7ad44b0b9d3ef83790f777cc4e
mgunnett <m-gunnett@onu.edu>
2025-02-04 13:38:04 -0500

    Completed P3C

--- src/lab3_PartC_GeneratingRandomNumbers/GeneratingRandomNumbers.java
+++ src/lab3_PartC_GeneratingRandomNumbers/GeneratingRandomNumbers.java
@@ -1,8 +1,17 @@
 package lab3_PartC_GeneratingRandomNumbers;
 
 import java.awt.EventQueue;
-
+import java.util.Random;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+/*
+ * Example of using the Random class from the java.util package for integer generation.
+ * 
+ * @author Megan E. Gunnett
+ * @version 1.0 - 4 February 2025
+ * */
 
 public class GeneratingRandomNumbers {
 
@@ -29,6 +38,21 @@ public class GeneratingRandomNumbers {
 	 */
 	public GeneratingRandomNumbers() {
 		initialize();
+		Random rnd = new Random();
+		int die1;
+		int die2;
+		
+		// show the user five rolls of the dice
+		for (int roll = 1; roll <= 5; roll++) {
+			die1 = rnd.nextInt(6) + 1;
+			die2 = rnd.nextInt(6) + 1;
+			
+			String title = "Roll #" + roll + " of 5";
+			String result = "<HTML>You have rolled a " + die1 + " and a " + die2
+					+ "<br>Total is " + (die1 + die2);
+			
+			JOptionPane.showMessageDialog(null, result, title, JOptionPane.PLAIN_MESSAGE);;
+		}
 	}
 
 	/**
